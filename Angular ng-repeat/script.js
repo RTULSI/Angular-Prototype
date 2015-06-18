d01beb295abbd1c146042bc8fecba12c51679d76
Rohit Tulsi <rtdkumar@gmail.com>
2015-06-18 12:46:00 -0500

    ng-repeat in Angular JS
    Angular JS ng-repeat

---
+++ Angular ng-repeat/script.js
@@ -0,0 +1,27 @@
+// Code goes here
+(function(){
+  var myApp = angular.module("myApp",[]);
+
+  var MainController = function($scope,$http){
+    var onRepos = function(response){
+      $scope.repo = response.data;
+    };
+    var onUserComplete = function(response){
+        $scope.user = response.data;
+        $http.get($scope.user.repos_url)
+          .then(onRepos,onError);
+      };
+      var onError = function(reason){
+        $scope.error = "Couldn't load the user data.";
+      }
+    $scope.message ="GithubViewer";
+    $scope.search = function(username){
+      $http.get("https://api.github.com/users/" + username)
+      .then(onUserComplete,onError);
+    };
+    
+  $scope.username = "ODETOCODE";
+  };
+  
+  myApp.controller("MainController",["$scope","$http",MainController]);
+}());
